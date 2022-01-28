26b7f6d33da2a498c5a01e94bfd1a82473792c22
kleidimilson <kleidimilson@gmail.com>
2022-01-28 11:53:53 -0300

    estrutura inicial do projeto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Header from './components/Header/Header'
+import './app.css'
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Filter() {
+  return (
+      <div>
+        <div>
+            
+        </div>
+      </div>
+  );
+}
+
+export default Filter;
