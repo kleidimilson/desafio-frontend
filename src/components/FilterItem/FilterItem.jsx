c981645f90900750fadbdc17f21551c5d92c9a53
kleidimilson <kleidimilson@gmail.com>
2022-01-30 12:28:19 -0300

    card

--- src/components/FilterItem/FilterItem.jsx
+++ src/components/FilterItem/FilterItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './FilterItem.css'
-import iconClose from 'https://firebasestorage.googleapis.com/v0/b/bdss-9e2e6.appspot.com/o/icon-remove.svg?alt=media&token=7a19a3c5-29e2-416c-8839-fc83a2899c90'
+import iconClose from '../../images/close.png'
 const FilterItem = ({item, _callback}) => {
     const removeFilter = () => {
         _callback(item)
