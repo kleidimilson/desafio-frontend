233623596c0e8d96f4a70ac5cf4d481a23d128aa
kleidimilson <kleidimilson@gmail.com>
2022-01-30 12:17:09 -0300

    ajuste nas imagens

--- src/components/FilterItem/FilterItem.jsx
+++ src/components/FilterItem/FilterItem.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import './FilterItem.css'
-import iconClose from '../../images/close.png'
+import iconClose from 'https://firebasestorage.googleapis.com/v0/b/bdss-9e2e6.appspot.com/o/icon-remove.svg?alt=media&token=7a19a3c5-29e2-416c-8839-fc83a2899c90'
 const FilterItem = ({item, _callback}) => {
-
     const removeFilter = () => {
         _callback(item)
     }
